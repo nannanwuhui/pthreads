f69fb191afdfab54d6fd9807877ac8c56a4342cc
Nan Zhao <nannanwuhui@aliyun.com>
2016-04-12 18:11:53 +0800

    用互斥量达到线程同步，但是加锁解锁有开销

---
+++ mutex.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+unsigned int g_cn = 0;
+pthread_mutex_t g_mtx;
+void* thread_proc(void* arg){
+    unsigned int i;
+    for(i = 0;i < 100000000;++i){
+        pthread_mutex_lock(&g_mtx);
+        ++g_cn;
+        pthread_mutex_unlock(&g_mtx);
+    }
+    return NULL;
+}
+int main(void){
+    pthread_mutex_init(&g_mtx,NULL);//内核中分配了一些资源
+    pthread_t tid1,tid2;
+    pthread_create(&tid1,NULL,thread_proc,NULL);
+    pthread_create(&tid2,NULL,thread_proc,NULL);
+    pthread_join(tid1,NULL);
+    pthread_join(tid2,NULL);
+    printf("g_cn = %u\n",g_cn);
+    pthread_mutex_destroy(&g_mtx);//释放资源
+    return 0;
+}
+#if 0
+这个程序执行完耗时较长，因为（100000000次的）开锁，解锁是有开销的。
+#endif
